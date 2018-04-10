7dc145b0024857a6984e4021dec1710490cb039e
Donald Tran <donaldtran@Donalds-MacBook-Air.local>
2018-04-10 09:52:51 -0500

    First Commit for Term Project

---
+++ app/Notes/readme.txt
@@ -0,0 +1,9 @@
+This readme file contains the following information:
+
+1 The name of the app
+
+2 A 1-to-5 line description of what the app does
+
+3 Special instructions needed to build the app (if none, please indicate "none").
+
+4 A list of known bugs.
---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/TeamBlakeDonJordan/AubieMainActivity.java
@@ -0,0 +1,13 @@
+package edu.auburn.eng.csse.comp3710.spring2018.TeamBlakeDonJordan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AubieMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aubie_main);
+    }
+}
