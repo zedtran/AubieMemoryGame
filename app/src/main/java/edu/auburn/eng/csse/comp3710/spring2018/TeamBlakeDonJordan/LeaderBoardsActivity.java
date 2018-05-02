36657eed5cf9bf13c7735096430322e8666dd755
zedtran <dzt0021@gmail.com>
2018-05-02 17:00:46 -0500

    Minor changes to Leaderboards Activity

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/TeamBlakeDonJordan/LeaderBoardsActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/TeamBlakeDonJordan/LeaderBoardsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.content.Context;
 
 
 
@@ -18,15 +19,20 @@ import android.util.Log;
 public class LeaderBoardsActivity extends Activity {
     private static final String DEBUG_TAG = "SimpleDB Log";
 
-    private ScoreboardDBHelper dbHelper = new ScoreboardDBHelper(this);
+    private ScoreboardDBHelper dbHelper;
 
     // Our database instance
     private SQLiteDatabase mDatabase;
+
+
+    protected Context mContext;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboards);
+        mContext = (Context) getBaseContext();
+        dbHelper = new ScoreboardDBHelper(mContext);
         if(savedInstanceState != null) { }
         Log.d(DEBUG_TAG, "onCreate()");
     }
